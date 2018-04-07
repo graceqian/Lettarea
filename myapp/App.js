cc5770549f023a53b383a270c97948ac17301a2d
Cassie Quach <cassieq120@gmail.com>
2018-04-07 19:52:09 -0400

    switching navigations

--- myapp/App.js
+++ myapp/App.js
@@ -34,7 +34,7 @@
 
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 class HomeScreen extends React.Component {
@@ -42,13 +42,43 @@ class HomeScreen extends React.Component {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
       </View>
     );
   }
 }
 
-export default StackNavigator({
-  Home: {
-    screen: HomeScreen,
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = StackNavigator(
+  {
+      Home: {
+        screen: HomeScreen,
+      },
+      Details: {
+          screen: DetailsScreen,
+      },
   },
-});
+  {
+    initialRouteName: 'Home',
+  }
+  );
+
+
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
